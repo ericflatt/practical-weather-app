8057f8943bbc39e2f065ff51a8db2136929df91f
ericflatt <eflatt18@gmail.com>
2021-07-12 19:02:22 -0400

    updates

---
+++ utils/WeatherAPIKey.js
@@ -0,0 +1,1 @@
+export const API_KEY = 'd9aa4aa88a9ce3653ec069d6081e0a01';
---
+++ utils/WeatherConditions.js
@@ -0,0 +1,51 @@
+export const weatherConditions = {
+    Rain: {
+      color: '#ffffff',
+      title: "It's Raining!!!",
+      subtitle: "It's wet outside",
+      icon: 'weather-rainy'
+    },
+    Clear: {
+      color: '#ffffff',
+      title: 'Not Raining',
+      subtitle: 'It is currently not raining',
+      icon: 'weather-sunny'
+    },
+    Thunderstorm: {
+      color: '#ffffff',
+      title: "It's Raining and thundering!!!",
+      subtitle: "It's wet outside and thundery",
+      icon: 'weather-lightning'
+    },
+    Clouds: {
+      color: '#ffffff',
+      title: 'Not Raining',
+      subtitle: 'It is currently not raining',
+      icon: 'weather-cloudy'
+    },
+  
+    Snow: {
+      color: '#ffffff',
+      title: 'Not Raining',
+      subtitle: 'It is currently not raining  but is snowing',
+      icon: 'weather-snowy'
+    },
+    Drizzle: {
+      color: '#ffffff',
+      title: 'Not Raining but drizzling',
+      subtitle: 'It is currently not raining',
+      icon: 'weather-rainy'
+    },
+    Haze: {
+      color: '#ffffff',
+      title: 'Not Raining',
+      subtitle: 'It is currently not raining',
+      icon: 'weather-haze'
+    },
+    Mist: {
+      color: '#ffffff',
+      title: 'Not Raining',
+      subtitle: "It is currently not raining",
+      icon: 'weather-fog'
+    }
+  };
